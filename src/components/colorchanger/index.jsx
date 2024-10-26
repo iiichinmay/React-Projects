c65a80872c994b108afff1c69a5bed70c179a807
Chinmay <155734661+iiichinmay@users.noreply.github.com>
2024-10-26 22:19:33 +0530

    Update index.jsx (Used Tailwind)

--- src/components/colorchanger/index.jsx
+++ src/components/colorchanger/index.jsx
@@ -4,44 +4,26 @@ export default function RandomColors() {
     const [color, setColor] = useState('white');
 
     const HexColor = () =>
-        setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+        setColor(() => `#${Math.floor(Math.random() * 16777215).toString(16)}`);
 
     const RgbColor = () =>
-        setColor(`rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`);
+        setColor(() => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
 
     return (
-        <div style={
-            {
-                backgroundColor: color,
-                height: '100vh',
-                width: '100vw',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }
-        }>
-            <div style={
-                {
-                    display: 'flex',
-                    gap: '10px',
-                }
-            }>
-
-                <button onClick={RgbColor} style={buttonStyle}>RGB Color</button>
-                <button onClick={() => (Math.random() < 0.5
-                    ? HexColor()
-                    : RgbColor())}
-                    style={buttonStyle}>Generate Random Color</button>
-                <button onClick={HexColor} style={buttonStyle}>HEX Color</button>
-
+        <div style={{ backgroundColor: color }} className="h-screen w-screen flex justify-center items-center">
+            <div className="flex gap-2">
+                <button onClick={RgbColor} className={buttonStyle}>RGB Color</button>
+                <button
+                    onClick={() => (Math.random() < 0.5 ? HexColor() : RgbColor())}
+                    className={buttonStyle}
+                >
+                    Generate Random Color
+                </button>
+                <button onClick={HexColor} className={buttonStyle}>HEX Color</button>
             </div>
         </div>
     );
 }
-const buttonStyle ={
-    border:'2px solid black',
-    borderRadius: '10px',
-    padding : '5px 20px',
-    cursor : 'pointer',
-    fontSize : '20px'
-}
+
+
+const buttonStyle = "border-2 border-black rounded-lg py-2 px-4 cursor-pointer text-lg hover:bg-gray-200 transition duration-200";
